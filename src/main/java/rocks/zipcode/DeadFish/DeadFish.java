0655bc86744f0e132901b9a7e1a49d10d92a3205
Ricky Kyte <137026079+rpk2301@users.noreply.github.com>
2023-09-18 18:02:45 -0400

    Made it a switch

--- src/main/java/rocks/zipcode/DeadFish/DeadFish.java
+++ src/main/java/rocks/zipcode/DeadFish/DeadFish.java
@@ -3,25 +3,24 @@ package rocks.zipcode.DeadFish;
 import java.util.ArrayList;
 
 public class DeadFish {
-
     public static int[] parse(String data) {
-       int ret=0;
-       ArrayList<Integer> arr = new ArrayList<>();
-       for(char charcter: data.toCharArray())
-       {
-         switch (charcter)
-         {
-             case 'd': ret--;
-             case 'i': ret++;
-             case 'o': arr.add(ret);
-             case 's': ret=(int)Math.pow(ret,2);
-             default: throw new IllegalArgumentException("Invalid Character");
-         }
-       }
-      int[] returnedArray = new int[arr.size()];
-       for(int i=0;i<arr.size();i++)
-       {
-           returnedArray[i]=arr.get(i);
-       }
-return returnedArray;
-}}
+        int ret=0;
+        ArrayList<Integer> arr = new ArrayList<>();
+        for(char charcter: data.toCharArray())
+        {
+            switch (charcter)
+            {
+                case 'd': ret--;break;
+                case 'i': ret++;break;
+                case 'o': arr.add(ret);break;
+                case 's': ret=(int)Math.pow(ret,2);break;
+                default:System.out.println("Invalid Expression");break;
+            }
+        }
+        int[] returnedArray = new int[arr.size()];
+        for(int i=0;i<arr.size();i++)
+        {
+            returnedArray[i]=arr.get(i);
+        }
+        return returnedArray;
+    }}
