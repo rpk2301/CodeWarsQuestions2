dafb9e928baa84551840162b69ed53af1258c484
Ricky Kyte <137026079+rpk2301@users.noreply.github.com>
2023-09-18 17:56:32 -0400

    Made it a switch

--- src/main/java/rocks/zipcode/DeadFish/DeadFish.java
+++ src/main/java/rocks/zipcode/DeadFish/DeadFish.java
@@ -7,12 +7,16 @@ public class DeadFish {
     public static int[] parse(String data) {
        int ret=0;
        ArrayList<Integer> arr = new ArrayList<>();
-       for(int i=0;i<data.length();i++)
+       for(char charcter: data.toCharArray())
        {
-           if(data.charAt(i)=='i'){ ret++;}
-           else if(data.charAt(i)=='d'){ ret--;}
-           else if(data.charAt(i)=='s'){ ret = (int) Math.pow(ret,2);}
-           else if(data.charAt(i)=='o'){ arr.add(ret);}
+         switch (charcter)
+         {
+             case 'd': ret--;
+             case 'i': ret++;
+             case 'o': arr.add(ret);
+             case 's': ret=(int)Math.pow(ret,2);
+             default: throw new IllegalArgumentException("Invalid Character");
+         }
        }
       int[] returnedArray = new int[arr.size()];
        for(int i=0;i<arr.size();i++)
